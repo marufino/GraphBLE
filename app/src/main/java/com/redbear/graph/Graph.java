c879a6c474f01d9e4766a3387a41861eee706623
Miguel Rufino <marufino@ncsu.edu>
2015-04-08 12:49:42 -0400

    First pass at trying to adapt code to chat with a ble nano( from redbearlab.com) to graph data instead

---
+++ app/src/main/java/com/redbear/graph/Graph.java
@@ -0,0 +1,298 @@
+package com.redbear.graph;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+
+public class Graph extends Activity {
+	private final static String TAG = Graph.class.getSimpleName();
+
+	public static final String EXTRAS_DEVICE = "EXTRAS_DEVICE";
+	private TextView tv = null;
+	private EditText et = null;
+	private Button btn = null;
+	private String mDeviceName;
+	private String mDeviceAddress;
+	private RBLService mBluetoothLeService;
+	private Map<UUID, BluetoothGattCharacteristic> map = new HashMap<UUID, BluetoothGattCharacteristic>();
+
+
+    GraphView graph;
+    LineGraphSeries<DataPoint> series1;
+    LineGraphSeries<DataPoint> series;
+
+
+	private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+		@Override
+		public void onServiceConnected(ComponentName componentName,
+				IBinder service) {
+			mBluetoothLeService = ((RBLService.LocalBinder) service)
+					.getService();
+			if (!mBluetoothLeService.initialize()) {
+				Log.e(TAG, "Unable to initialize Bluetooth");
+				finish();
+			}
+			// Automatically connects to the device upon successful start-up
+			// initialization.
+			mBluetoothLeService.connect(mDeviceAddress);
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName componentName) {
+			mBluetoothLeService = null;
+		}
+	};
+
+	private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			final String action = intent.getAction();
+
+			if (RBLService.ACTION_GATT_DISCONNECTED.equals(action)) {
+			} else if (RBLService.ACTION_GATT_SERVICES_DISCOVERED
+					.equals(action)) {
+				getGattService(mBluetoothLeService.getSupportedGattService());
+			} else if (RBLService.ACTION_DATA_AVAILABLE.equals(action)) {
+				displayData(intent.getByteArrayExtra(RBLService.EXTRA_DATA));
+			}
+		}
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second);
+
+        series1 = new LineGraphSeries<DataPoint>();
+
+        graph = (GraphView) findViewById(R.id.graph);
+		tv = (TextView) findViewById(R.id.textView);
+		tv.setMovementMethod(ScrollingMovementMethod.getInstance());
+		et = (EditText) findViewById(R.id.editText);
+		btn = (Button) findViewById(R.id.send);
+		btn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				BluetoothGattCharacteristic characteristic = map
+						.get(RBLService.UUID_BLE_SHIELD_TX);
+
+				String str = et.getText().toString();
+				byte b = 0x00;
+				byte[] tmp = str.getBytes();
+				byte[] tx = new byte[tmp.length + 1];
+				tx[0] = b;
+				for (int i = 1; i < tmp.length + 1; i++) {
+					tx[i] = tmp[i - 1];
+				}
+
+				characteristic.setValue(tx);
+				mBluetoothLeService.writeCharacteristic(characteristic);
+
+				et.setText("");
+			}
+		});
+
+		Intent intent = getIntent();
+
+		mDeviceAddress = intent.getStringExtra(Device.EXTRA_DEVICE_ADDRESS);
+		mDeviceName = intent.getStringExtra(Device.EXTRA_DEVICE_NAME);
+
+
+        getActionBar().setTitle(mDeviceName);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+		Intent gattServiceIntent = new Intent(this, RBLService.class);
+		bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
+
+
+        series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 0),
+        });
+
+        LineGraphSeries<DataPoint> series2 = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+
+        series2.setColor(Color.GREEN);
+
+        graph.addSeries(series);
+        graph.addSeries(series2);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			mBluetoothLeService.disconnect();
+			mBluetoothLeService.close();
+
+			System.exit(0);
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+
+		unregisterReceiver(mGattUpdateReceiver);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		mBluetoothLeService.disconnect();
+		mBluetoothLeService.close();
+
+		System.exit(0);
+	}
+/*
+	private void displayData(byte[] byteArray) {
+		if (byteArray != null) {
+			String data = new String(byteArray);
+			tv.append(data);
+			// find the amount we need to scroll. This works by
+			// asking the TextView's internal layout for the position
+			// of the final line and then subtracting the TextView's height
+			final int scrollAmount = tv.getLayout().getLineTop(
+					tv.getLineCount())
+					- tv.getHeight();
+			// if there is no need to scroll, scrollAmount will be <=0
+			if (scrollAmount > 0)
+				tv.scrollTo(0, scrollAmount);
+			else
+				tv.scrollTo(0, 0);
+		}
+	}*/
+
+    private void displayData(byte[] byteArray) {
+
+        if (byteArray != null) {
+            String data = new String(byteArray);
+
+
+            tv.setText(data);
+            //if (isFloatPair(data)) {
+
+            if (data.contains(","))
+            {
+
+                String[] point = data.split(",");
+                float x = Float.parseFloat(point[0]);
+                float y = Float.parseFloat(point[1]);
+
+                Log.d("strIncom", data);
+                Log.d("X: ",point[0]);
+                Log.d("Y:",point[1]);
+
+                series.appendData(new DataPoint(x, y),true,10);
+
+                graph.addSeries(series);
+                graph.getViewport().setXAxisBoundsManual(true);
+                graph.getViewport().setMinX(0);
+                graph.getViewport().setMaxX(40);
+            }
+        }
+
+
+        /*
+        if (strIncom.indexOf('.')==2 && strIncom.indexOf('s')==0){
+            strIncom = strIncom.replace("s", "");
+            if (isFloatNumber(strIncom)){
+                Series.appendData(new GraphViewData(graph2LastXValue,Double.parseDouble(strIncom)),AutoScrollX);
+
+                Series.appendData();
+
+                graph2LastXValue += 0.1;
+
+
+
+                if(Lock == true)
+                    graphView.setViewPort(0, Xview);
+                else
+                    graphView.setViewPort(graph2LastXValue-Xview, Xview);
+
+                //refresh
+                GraphView.removeView(graphView);
+                GraphView.addView(graphView);
+
+            }
+        }*/
+    }
+
+    public boolean isFloatPair(String num){
+        return true;
+        /*//Log.d("checkfloatNum", num);
+        try{
+            Double.parseDouble(num);
+        } catch(NumberFormatException nfe) {
+            return false;
+        }
+        return true;*/
+    }
+
+	private void getGattService(BluetoothGattService gattService) {
+		if (gattService == null)
+			return;
+
+		BluetoothGattCharacteristic characteristic = gattService
+				.getCharacteristic(RBLService.UUID_BLE_SHIELD_TX);
+		map.put(characteristic.getUuid(), characteristic);
+
+		BluetoothGattCharacteristic characteristicRx = gattService
+				.getCharacteristic(RBLService.UUID_BLE_SHIELD_RX);
+		mBluetoothLeService.setCharacteristicNotification(characteristicRx,
+				true);
+		mBluetoothLeService.readCharacteristic(characteristicRx);
+	}
+
+	private static IntentFilter makeGattUpdateIntentFilter() {
+		final IntentFilter intentFilter = new IntentFilter();
+
+		intentFilter.addAction(RBLService.ACTION_GATT_CONNECTED);
+		intentFilter.addAction(RBLService.ACTION_GATT_DISCONNECTED);
+		intentFilter.addAction(RBLService.ACTION_GATT_SERVICES_DISCOVERED);
+		intentFilter.addAction(RBLService.ACTION_DATA_AVAILABLE);
+
+		return intentFilter;
+	}
+}
